f4ed60967e8784da84a45bf90acfbd861e3bb561
benrawn <ben.rawn@gmail.com>
2012-12-27 11:00:39 +0100

    payments-- intermediate

--- app/Controller/users_controller.php
+++ app/Controller/users_controller.php
@@ -362,24 +362,26 @@ class UsersController extends AppController{
 		else $this->redirect('/dashboard/');
 	}
 	
-	function process_payment(){
+	function process_payment($user_id){
+		$user = $this->User->findById($id);
+		
 		$trxnProperties = array(
 		  "User_Name"=>"",
 		  "Secure_AuthResult"=>"",
 		  "Ecommerce_Flag"=>"",
 		  "XID"=>"",
-		  "ExactID"=>$_POST["ddlPOS_ExactID"],				    //Payment Gateway
+		  "ExactID"=>'A28460-01',
 		  "CAVV"=>"",
-		  "Password"=>"",					                //Gateway Password
+		  "Password"=>"14r0z18j",
 		  "CAVV_Algorithm"=>"",
-		  "Transaction_Type"=>$_POST["ddlPOS_Transaction_Type"],//Transaction Code I.E. Purchase="00" Pre-Authorization="01" etc.
-		  "Reference_No"=>$_POST["tbPOS_Reference_No"],
-		  "Customer_Ref"=>$_POST["tbPOS_Customer_Ref"],
-		  "Reference_3"=>$_POST["tbPOS_Reference_3"],
-		  "Client_IP"=>"",					                    //This value is only used for fraud investigation.
-		  "Client_Email"=>$_POST["tb_Client_Email"],			//This value is only used for fraud investigation.
-		  "Language"=>$_POST["ddlPOS_Language"],				//English="en" French="fr"
-		  "Card_Number"=>$_POST["tbPOS_Card_Number"],		    //For Testing, Use Test#s VISA="4111111111111111" MasterCard="5500000000000004" etc.
+		  "Transaction_Type"=>"00",
+		  "Reference_No"=>"",
+		  "Customer_Ref"=>"{$user['User']['id']}",
+		  "Reference_3"=>"",
+		  "Client_IP"=>"",
+		  "Client_Email"=>$_POST["tb_Client_Email"],
+		  "Language"=>'en',
+		  "Card_Number"=>$_POST["card_num"],		    //For Testing, Use Test#s VISA="4111111111111111" MasterCard="5500000000000004" etc.
 		  "Expiry_Date"=>$_POST["ddlPOS_Expiry_Date_Month"] . $_POST["ddlPOS_Expiry_Date_Year"],//This value should be in the format MM/YY.
 		  "CardHoldersName"=>$_POST["tbPOS_CardHoldersName"],
 		  "Track1"=>"",
@@ -427,5 +429,30 @@ class UsersController extends AppController{
 	function session_logout(){
 		$this->layout = 'ajax';
 	}
+}
+
+/* for payment transactions */
+class SoapClientHMAC extends SoapClient {
+  public function __doRequest($request, $location, $action, $version, $one_way = NULL) {
+	global $context;
+	$hmackey = ""; // <-- Insert your HMAC key here
+	$keyid = ""; // <-- Insert the Key ID here
+	$hashtime = date("c");
+	$hashstr = "POST\ntext/xml; charset=utf-8\n" . sha1($request) . "\n" . $hashtime . "\n" . parse_url($location,PHP_URL_PATH);
+	$authstr = base64_encode(hash_hmac("sha1",$hashstr,$hmackey,TRUE));
+	if (version_compare(PHP_VERSION, '5.3.11') == -1) {
+		ini_set("user_agent", "PHP-SOAP/" . PHP_VERSION . "\r\nAuthorization: GGE4_API " . $keyid . ":" . $authstr . "\r\nx-gge4-date: " . $hashtime . "\r\nx-gge4-content-sha1: " . sha1($request));
+	} else {
+		stream_context_set_option($context,array("http" => array("header" => "authorization: GGE4_API " . $keyid . ":" . $authstr . "\r\nx-gge4-date: " . $hashtime . "\r\nx-gge4-content-sha1: " . sha1($request))));
+	}
+    return parent::__doRequest($request, $location, $action, $version, $one_way);
+  }
+  
+  public function SoapClientHMAC($wsdl, $options = NULL) {
+	global $context;
+	$context = stream_context_create();
+	$options['stream_context'] = $context;
+	return parent::SoapClient($wsdl, $options);
+  }
 }
 ?>
