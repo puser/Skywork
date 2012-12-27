84e312f65034a818b4491506bfc3b8bd3ed0d786
benrawn <ben.rawn@gmail.com>
2012-12-27 14:00:35 +0100

    payments-- intermediate

--- app/Controller/users_controller.php
+++ app/Controller/users_controller.php
@@ -362,55 +362,29 @@ class UsersController extends AppController{
 		else $this->redirect('/dashboard/');
 	}
 	
-	function process_payment($user_id){
-		$user = $this->User->findById($id);
+	function process_payment(){
+		$user = $this->User->findById($_SESSION['User']['id']);
 		
 		$trxnProperties = array(
-		  "User_Name"=>"",
-		  "Secure_AuthResult"=>"",
-		  "Ecommerce_Flag"=>"",
-		  "XID"=>"",
-		  "ExactID"=>'A28460-01',
-		  "CAVV"=>"",
-		  "Password"=>"14r0z18j",
-		  "CAVV_Algorithm"=>"",
-		  "Transaction_Type"=>"00",
-		  "Reference_No"=>"",
-		  "Customer_Ref"=>"{$user['User']['id']}",
-		  "Reference_3"=>"",
-		  "Client_IP"=>"",
-		  "Client_Email"=>$_POST["tb_Client_Email"],
+		  // "ExactID"=>'A28460-01', // PRODUCTION
+			"ExactID"=>'AD1689-05', // TESTING
+		  // "Password"=> "14r0z18j", // PRODUCTION
+		  "Password"=> "181jw558", // TESTING
+			"Transaction_Type"=>"00",
+		  "Customer_Ref"=>$user['User']['id'],
+		  "Client_Email"=>$user['User']['email'],
 		  "Language"=>'en',
-		  "Card_Number"=>$_POST["card_num"],		    //For Testing, Use Test#s VISA="4111111111111111" MasterCard="5500000000000004" etc.
-		  "Expiry_Date"=>$_POST["ddlPOS_Expiry_Date_Month"] . $_POST["ddlPOS_Expiry_Date_Year"],//This value should be in the format MM/YY.
-		  "CardHoldersName"=>$_POST["tbPOS_CardHoldersName"],
-		  "Track1"=>"",
-		  "Track2"=>"",
-		  "Authorization_Num"=>$_POST["tbPOS_Authorization_Num"],
-		  "Transaction_Tag"=>$_POST["tbPOS_Transaction_Tag"],
-		  "DollarAmount"=>$_POST["tbPOS_DollarAmount"],
-		  "VerificationStr1"=>$_POST["tbPOS_VerificationStr1"],
-		  "VerificationStr2"=>"",
-		  "CVD_Presence_Ind"=>"",
-		  "Secure_AuthRequired"=>"",
-		  "Currency"=>"",
-		  "PartialRedemption"=>"",
-
-		  // Level 2 fields 
-		  "ZipCode"=>$_POST["tbPOS_ZipCode"],
-		  "Tax1Amount"=>$_POST["tbPOS_Tax1Amount"],
-		  "Tax1Number"=>$_POST["tbPOS_Tax1Number"],
-		  "Tax2Amount"=>$_POST["tbPOS_Tax2Amount"],
-		  "Tax2Number"=>$_POST["tbPOS_Tax2Number"],
-
-		  "SurchargeAmount"=>$_POST["tbPOS_SurchargeAmount"],	//Used for debit transactions only
-		  "PAN"=>$_POST["tbPOS_PAN"]							//Used for debit transactions only
+		  "Card_Number"=>$_POST["card_num"], // Test #s: VISA="4111111111111111" MasterCard="5500000000000004"
+		  "Expiry_Date"=>$_POST["card_expiry"], // format as MM/YY.
+		  "CardHoldersName"=>$_POST["card_name"],
+		  "DollarAmount"=> (@$user['User']['account_tier'] == 'PLATINUM' ? 19.99 : 9.99),
+		  "VerificationStr1"=>"{$_POST['street']}|{$_POST['zip']}|{$_POST['city']}|{$_POST['state']}|{$_POST['country']}",
+		  "VerificationStr2"=>$_POST['card_code'],
+		  "CVD_Presence_Ind"=>($_POST['card_code'] ? 1 : 0)
 		  );
-
 
 		$client = new SoapClientHMAC("https://api.demo.globalgatewaye4.firstdata.com/transaction/v12/wsdl");
 		$trxnResult = $client->SendAndCommit($trxnProperties);
-
 
 		if(@$client->fault){
 		    // there was a fault, inform
@@ -418,6 +392,15 @@ class UsersController extends AppController{
 		    print "String: {$client->faultstring} </B>";
 		    $trxnResult["CTR"] = "There was an error while processing. No TRANSACTION DATA IN CTR!";
 		}
+		
+		foreach($trxnResult as $key=>$value){
+			if ($key == "CTR") {
+				$value = nl2br($value);
+				print $value;
+			}
+		}
+		
+		die();
 	}
 	
 	// deauthenticate
@@ -434,25 +417,31 @@ class UsersController extends AppController{
 /* for payment transactions */
 class SoapClientHMAC extends SoapClient {
   public function __doRequest($request, $location, $action, $version, $one_way = NULL) {
-	global $context;
-	$hmackey = ""; // <-- Insert your HMAC key here
-	$keyid = ""; // <-- Insert the Key ID here
-	$hashtime = date("c");
-	$hashstr = "POST\ntext/xml; charset=utf-8\n" . sha1($request) . "\n" . $hashtime . "\n" . parse_url($location,PHP_URL_PATH);
-	$authstr = base64_encode(hash_hmac("sha1",$hashstr,$hmackey,TRUE));
-	if (version_compare(PHP_VERSION, '5.3.11') == -1) {
-		ini_set("user_agent", "PHP-SOAP/" . PHP_VERSION . "\r\nAuthorization: GGE4_API " . $keyid . ":" . $authstr . "\r\nx-gge4-date: " . $hashtime . "\r\nx-gge4-content-sha1: " . sha1($request));
-	} else {
-		stream_context_set_option($context,array("http" => array("header" => "authorization: GGE4_API " . $keyid . ":" . $authstr . "\r\nx-gge4-date: " . $hashtime . "\r\nx-gge4-content-sha1: " . sha1($request))));
-	}
+		global $context;
+		/* PRODUCTION */
+		// $hmackey = "Koyr_iZIq3HcaRqIYx9JHtT82xC1LA3C";
+		// $keyid = "13136";
+		
+		/* TESTING */
+		$hmackey = "TuloMl3mBZg4si2~k6sKUGIkh29ch1kU";
+		$keyid = "11318";
+		
+		$hashtime = date("c");
+		$hashstr = "POST\ntext/xml; charset=utf-8\n" . sha1($request) . "\n" . $hashtime . "\n" . parse_url($location,PHP_URL_PATH);
+		$authstr = base64_encode(hash_hmac("sha1",$hashstr,$hmackey,TRUE));
+		if (version_compare(PHP_VERSION, '5.3.11') == -1) {
+			ini_set("user_agent", "PHP-SOAP/" . PHP_VERSION . "\r\nAuthorization: GGE4_API " . $keyid . ":" . $authstr . "\r\nx-gge4-date: " . $hashtime . "\r\nx-gge4-content-sha1: " . sha1($request));
+		} else {
+			stream_context_set_option($context,array("http" => array("header" => "authorization: GGE4_API " . $keyid . ":" . $authstr . "\r\nx-gge4-date: " . $hashtime . "\r\nx-gge4-content-sha1: " . sha1($request))));
+		}
     return parent::__doRequest($request, $location, $action, $version, $one_way);
   }
   
   public function SoapClientHMAC($wsdl, $options = NULL) {
-	global $context;
-	$context = stream_context_create();
-	$options['stream_context'] = $context;
-	return parent::SoapClient($wsdl, $options);
+		global $context;
+		$context = stream_context_create();
+		$options['stream_context'] = $context;
+		return parent::SoapClient($wsdl, $options);
   }
 }
 ?>
---
+++ app/View/Users/update_payment.ctp
@@ -0,0 +1,11 @@
+<form action="/users/process_payment/" method="POST">
+	number: <input type="text" name="card_num" />
+	expiry: <input type="text" name="card_expiry" />
+	name: <input type="text" name="card_name" />
+	cvv: <input type="text" name="card_code" />
+	street: <input type="text" name="street" />
+	zip: <input type="text" name="zip" />
+	city: <input type="text" name="city" />
+	state: <input type="text" name="state" />
+	country: <input type="text" name="country" />
+</form>
