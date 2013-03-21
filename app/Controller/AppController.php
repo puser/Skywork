a5c3d2b7e718d933ac78ad2af072b6a97691cc35
benrawn <ben.rawn@gmail.com>
2013-03-21 15:20:38 -0600

    redirect to skywork

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -7,6 +7,16 @@ class AppController extends Controller{
   }
 	
 	function checkAuth($ajax = false){
+		if(!(stristr($_SERVER['SERVER_NAME'],'edge') || stristr($_SERVER['SERVER_NAME'],'staging')) && stristr($_SERVER['SERVER_NAME'],'puentes')){
+			header('Location: http://doskywork.com' . $_SERVER['REQUEST_URI']);
+			exit();
+		}
+		
+		if(@$_SERVER['HTTPS'] != 'on' && !(stristr($_SERVER['SERVER_NAME'],'edge') || stristr($_SERVER['SERVER_NAME'],'staging')) && stristr($_SERVER['HTTP_HOST'],'puentes')){
+			header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+			exit();
+		}
+		
 		if(!$this->Session->check('User')){
 			if($ajax) header('Location: /users/session_logout/');
 			else header('Location: /login/');
